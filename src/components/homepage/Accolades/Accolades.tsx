4cab66b819a2a1a846e3d47e1d2bddb10eb1a6ab
Charles <knappcharles@icloud.com>
2023-08-15 10:29:09 -0400

    Fixed homepage testi sizing

--- src/components/homepage/Accolades/Accolades.tsx
+++ src/components/homepage/Accolades/Accolades.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Accolades = () => {
   return (
     <div className='flex h-full flex-wrap items-center justify-evenly gap-10 bg-[rgba(255,173,173,0.2)] bg-opacity-30 py-20'>
-      <div className='h-[180px] w-[180px] bg-blue-200'></div>
-      <div className='h-[180px] w-[180px] bg-blue-200'></div>
-      <div className='h-[180px] w-[180px] bg-blue-200'></div>
-      <div className='h-[180px] w-[180px] bg-blue-200'></div>
-      <div className='h-[180px] w-[180px] bg-blue-200'></div>
+      <div className='bg-primary-50 h-[150px] w-[150px] rounded-2xl'></div>
+      <div className='bg-primary-50 h-[150px] w-[150px] rounded-2xl'></div>
+      <div className='bg-primary-50 h-[150px] w-[150px] rounded-2xl'></div>
+      <div className='bg-primary-50 h-[150px] w-[150px] rounded-2xl'></div>
+      <div className='bg-primary-50 h-[150px] w-[150px] rounded-2xl'></div>
     </div>
   );
 };
--- src/components/homepage/Testimonials/Testimonials.tsx
+++ src/components/homepage/Testimonials/Testimonials.tsx
@@ -8,22 +8,25 @@ import person3 from '/public/images/person3.png';
 const testimonials = [
   {
     img: person1, // Update with your image paths
-    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     name: 'Jessie J',
-    role: 'Acme LTD'
+    role: 'Acme LTD',
   },
   {
     img: person2, // Update with your image paths
-    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     name: 'Nick V',
-    role: 'Malika Inc.'
+    role: 'Malika Inc.',
   },
   {
     img: person3, // Update with your image paths
-    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     name: 'Amelia W',
-    role: 'Panda AI'
-  }
+    role: 'Panda AI',
+  },
 ];
 
 const Testimonials = () => {
@@ -55,56 +58,67 @@ const Testimonials = () => {
   }, []);
 
   return (
-    <div className='w-full h-full my-32 flex flex-col justify-center items-center'>
-      <div className='text-center max-w-[900px] flex flex-col gap-6 mb-16'>
-        <h2 className='font-junge lg:text-[3.25rem] text-[2.75rem] leading-[1.1em] text-primary-500'>Hear what my clients have to say about coaching.</h2>
-        <p className='leading-[1.25em] px-4 lg:text-[1.75rem] text-[1.5rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <div className='my-32 flex h-full w-full flex-col items-center justify-center'>
+      <div className='mb-16 flex max-w-[1200px] flex-col gap-6 text-center'>
+        <h2 className='font-junge text-primary-500 text-[3rem] leading-[1.1em]'>
+          Coaching Success Stories
+        </h2>
+        <p className='m-auto w-[70ch] px-4 text-[1.5rem] leading-[1.25em]'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
       </div>
-      <div className="w-full max-w-3xl mx-auto text-center">
+      <div className='mx-auto w-full max-w-3xl text-center'>
         {/* Container for testimonial images */}
-        <div className="relative h-32">
+        <div className='relative h-32'>
           {/* Background circle */}
-          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[480px] h-[480px] pointer-events-none before:absolute before:inset-0 before:bg-gradient-to-b before:from-primary-500/25 before:via-primary-500/5 before:via-25% before:to-primary-500/0 before:to-75% before:rounded-full before:-z-10">
+          <div className='before:from-primary-500/25 before:via-primary-500/5 before:to-primary-500/0 pointer-events-none absolute left-1/2 top-0 h-[480px] w-[480px] -translate-x-1/2 before:absolute before:inset-0 before:-z-10 before:rounded-full before:bg-gradient-to-b before:via-25% before:to-75%'>
             {/* Container for testimonial image masks */}
-            <div className="h-32 flex [mask-image:_linear-gradient(0deg,transparent,theme(colors.white)_20%,theme(colors.white))]">
+            <div className='flex h-32 [mask-image:_linear-gradient(0deg,transparent,theme(colors.white)_20%,theme(colors.white))]'>
               {/* Map through testimonials and display images */}
               {testimonials.map((testimonial, index) => (
                 <Transition
                   key={index}
                   show={active === index}
-                  className="absolute inset-0 h-full -z-10"
-                  enter="transition ease-[cubic-bezier(0.68,-0.3,0.32,1)] duration-700 order-first"
-                  enterFrom="opacity-0 -rotate-[60deg]"
-                  enterTo="opacity-100 rotate-0"
-                  leave="transition ease-[cubic-bezier(0.68,-0.3,0.32,1)] duration-700"
-                  leaveFrom="opacity-100 rotate-0"
-                  leaveTo="opacity-0 rotate-[60deg]"
+                  className='absolute inset-0 -z-10 h-full'
+                  enter='transition ease-[cubic-bezier(0.68,-0.3,0.32,1)] duration-700 order-first'
+                  enterFrom='opacity-0 -rotate-[60deg]'
+                  enterTo='opacity-100 rotate-0'
+                  leave='transition ease-[cubic-bezier(0.68,-0.3,0.32,1)] duration-700'
+                  leaveFrom='opacity-100 rotate-0'
+                  leaveTo='opacity-0 rotate-[60deg]'
                 >
                   {/* Display testimonial image */}
-                  <Image className="relative top-11 left-1/2 -translate-x-1/2 rounded-full" src={testimonial.img} width={100} height={100} alt={testimonial.name} />
+                  <Image
+                    className='relative left-1/2 top-11 -translate-x-1/2 rounded-full'
+                    src={testimonial.img}
+                    width={100}
+                    height={100}
+                    alt={testimonial.name}
+                  />
                 </Transition>
               ))}
             </div>
           </div>
         </div>
         {/* Text section */}
-        <div className="mb-9 transition-all duration-150 delay-300 ease-in-out">
-          <div className="relative flex flex-col" ref={testimonialsRef}>
+        <div className='mb-9 transition-all delay-300 duration-150 ease-in-out'>
+          <div className='relative flex flex-col' ref={testimonialsRef}>
             {/* Map through testimonials and display quotes */}
             {testimonials.map((testimonial, index) => (
               <Transition
                 key={index}
                 show={active === index}
-                enter="transition ease-in-out duration-500 delay-200 order-first"
-                enterFrom="opacity-0 -translate-x-4"
-                enterTo="opacity-100 translate-x-0"
-                leave="transition ease-out duration-300 delay-300 absolute"
-                leaveFrom="opacity-100 translate-x-0"
-                leaveTo="opacity-0 translate-x-4"
+                enter='transition ease-in-out duration-500 delay-200 order-first'
+                enterFrom='opacity-0 -translate-x-4'
+                enterTo='opacity-100 translate-x-0'
+                leave='transition ease-out duration-300 delay-300 absolute'
+                leaveFrom='opacity-100 translate-x-0'
+                leaveTo='opacity-0 translate-x-4'
                 beforeEnter={() => heightFix()} // Call a function before entering transition
               >
                 {/* Display testimonial quote */}
-                <div className="text-2xl font-bold text-slate-900 before:content-['\201C'] after:content-['\201D'] font-junge">
+                <div className="text-2xl font-normal text-gray-500 before:content-['\201C'] after:content-['\201D']">
                   {testimonial.quote}
                 </div>
               </Transition>
@@ -112,13 +126,20 @@ const Testimonials = () => {
           </div>
         </div>
         {/* Buttons for selecting testimonials */}
-        <div className="flex flex-wrap justify-center -m-1.5">
+        <div className='-m-1.5 flex flex-wrap justify-center'>
           {/* Map through testimonials and display selection buttons */}
           {testimonials.map((testimonial, index) => (
             <button
               key={index}
-              className={`inline-flex justify-center whitespace-nowrap rounded-xl px-5 py-3 m-2 font-junge font-semibold shadow-sm focus-visible:outline-none focus-visible:ring focus-visible:ring-primary-300 dark:focus-visible:ring-gray-700 transition-colors duration-150 ${active === index ? 'bg-primary-500 text-white shadow-primary-950/10' : 'bg-white hover:bg-primary-100 text-slate-900'}`}
-              onClick={() => { setActive(index); setAutorotate(false); }} // Set active testimonial and disable autorotation
+              className={`focus-visible:ring-primary-300 m-2 inline-flex justify-center whitespace-nowrap rounded-xl px-5 py-3 font-semibold shadow-sm transition-colors duration-150 focus-visible:outline-none focus-visible:ring dark:focus-visible:ring-gray-700 ${
+                active === index
+                  ? 'bg-primary-500 shadow-primary-950/10 text-white'
+                  : 'hover:bg-primary-100 bg-white text-gray-500'
+              }`}
+              onClick={() => {
+                setActive(index);
+                setAutorotate(false);
+              }} // Set active testimonial and disable autorotation
             >
               {/* Display testimonial name and role */}
               <span>{testimonial.name} </span> <span>{testimonial.role}</span>
@@ -127,7 +148,7 @@ const Testimonials = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-  export default Testimonials;
+export default Testimonials;
