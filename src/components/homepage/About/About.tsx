cfbede237ee0b61b8dd19ee98338d340d7b575bb
Charles <knappcharles@icloud.com>
2023-08-15 09:29:02 -0400

    Homepage gradient added and redesigned hero

--- src/components/homepage/About/About.tsx
+++ src/components/homepage/About/About.tsx
@@ -1,42 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 const aboutCheckMarks = [
   'Lorem ipsum dolor sit amet',
   'Lorem ipsum dolor sit amet',
   'Lorem ipsum dolor sit amet',
-]
+];
 
 const About = () => {
   return (
-    <div className='bg-primary-500 bg-opacity-30 w-full h-[90vh]'>
-      <div className='w-full h-[90vh] flex sm:flex-row flex-col justify-evenly items-center sm:py-0 py-10'>
-        <div className='w-full sm:w-1/2 h-full max-w-[800px] max-h-[80%] flex justify-center items-center'>
-          <div className='bg-gray-400 w-[80%] h-full max-w-[800px] rounded-[7rem] sm:rounded-r-none sm:rounded-tl-[7rem] rounded-tl-none'>
-            <img src="" alt="" />
+    <div className='h-[90vh] w-full bg-[rgba(255,173,173,0.2)] bg-opacity-30'>
+      <div className='flex h-[90vh] w-full flex-col items-center justify-evenly py-10 sm:flex-row sm:py-0'>
+        <div className='flex h-full max-h-[80%] w-full max-w-[800px] items-center justify-center sm:w-1/2'>
+          <div className='h-full w-[80%] max-w-[800px] rounded-[7rem] rounded-tl-none bg-gray-400 sm:rounded-r-none sm:rounded-tl-[7rem]'>
+            <img src='' alt='' />
           </div>
         </div>
-        <div className='w-full sm:w-1/2 h-full max-w-[800px] max-h-[80%] flex flex-col sm:justify-evenly items-center sm:pr-10 sm:px-0 px-10 text-gray-700 sm:pt-0 pt-10'>
+        <div className='flex h-full max-h-[80%] w-full max-w-[800px] flex-col items-center px-10 pt-10 text-gray-700 sm:w-1/2 sm:justify-evenly sm:px-0 sm:pr-10 sm:pt-0'>
           <div>
-            <h1 className='font-junge font-medium xl:text-[4.5em] text-[3.5em] sm:leading-[1em] leading-[1.25em]'>
-              Expert Educator: <span className='text-primary-500 ml-0 md:ml-6'>Shiren Rattigan</span>
+            <h1 className='font-junge text-[3.5em] font-medium leading-[1.25em] sm:leading-[1em] xl:text-[4.5em]'>
+              Expert Educator:{' '}
+              <span className='text-primary-500 ml-0 md:ml-6'>
+                Shiren Rattigan
+              </span>
             </h1>
-            <p className='w-full xl:text-[1.5em] text-[1em] leading-[1.75em]'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <p className='w-full text-[1em] leading-[1.75em] xl:text-[1.5em]'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
           </div>
-          <div className='w-full flex flex-col gap-4 mb-4'>
+          <div className='mb-4 flex w-full flex-col gap-4'>
             {aboutCheckMarks.map((item, index) => (
-              <div className='w-full flex items-center gap-8 justify-center sm:justify-start'>
-                <div className='w-[40px] h-[40px] bg-primary-500 rounded-full'></div>
-                <p className='lg:text-[26px] text-[16px] font-semibold'>Lorem ipsum dolor sit amet</p>
+              <div className='flex w-full items-center justify-center gap-8 sm:justify-start'>
+                <div className='bg-primary-500 h-[40px] w-[40px] rounded-full'></div>
+                <p className='text-[16px] font-semibold lg:text-[26px]'>
+                  Lorem ipsum dolor sit amet
+                </p>
               </div>
             ))}
           </div>
-          <div className='w-full text-[1.3em] font-junge font-semibold flex lg:gap-10 gap-4 lg:flex-row flex-col'>
-            <button className='border-2 border-primary-500 rounded-xl py-4 px-14 font-black font-junge bg-primary-500 text-white hover:shadow-md hover:-translate-y-1 transition-all'>
+          <div className='font-junge flex w-full flex-col gap-4 text-[1.3em] font-semibold lg:flex-row lg:gap-10'>
+            <button className='border-primary-500 font-junge bg-primary-500 rounded-xl border-2 px-14 py-4 font-black text-white transition-all hover:-translate-y-1 hover:shadow-md'>
               Start with Shiren
             </button>
-            <div className='lg:w-1/2 h-full flex items-center lg:ml-0 ml-2'>
+            <div className='ml-2 flex h-full items-center lg:ml-0 lg:w-1/2'>
               <a href='/contact' className='underline'>
                 Find out more about Shiren now! →
               </a>
@@ -45,7 +51,7 @@ const About = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/homepage/Hero/Hero.tsx
+++ src/components/homepage/Hero/Hero.tsx
@@ -2,29 +2,43 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='w-full h-[90vh] flex md:flex-row flex-col-reverse justify-center items-center 2xl:px-0 md:px-12 px-4 2xl:gap-24 md:gap-16'>
-      <div className='2xl:w-1/3 md:w-1/2 w-full h-full flex flex-col justify-center items-center text-gray-700'>
-        <h1 className='font-junge font-medium xl:text-[4.5em] text-[3.5em] leading-[1em]'>
-          Your <span className='text-primary-500'>Educator Journey</span> Starts Here
+    <div className='relative flex h-[90vh] w-full flex-col-reverse items-center justify-center overflow-hidden px-4 md:flex-row md:gap-16 md:px-12 2xl:gap-24 2xl:px-0'>
+      <div className='absolute inset-0 origin-bottom-left transform bg-gradient-to-b from-white to-[rgba(255,173,173,0.2)]'></div>
+      <div className='relative z-10 flex h-full w-full flex-col items-center justify-center text-gray-700 md:w-1/2 2xl:w-1/3'>
+        <h1 className='font-junge text-[3.5em] font-medium leading-[1em] xl:text-[4.5em]'>
+          Your <span className='text-primary-500'>Educator Journey</span> Starts
+          Here
         </h1>
-        <p className='w-full my-[6%] xl:text-[1.5em] text-[1.25em] leading-[1.75em]'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <p className='my-[6%] w-full text-[1.5em] leading-[1.25em]'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
         </p>
-        <div className='w-full xl:text-[1.3em] text-[1.1em] font-junge font-semibold flex lg:gap-6 lg:flex-row flex-col'>
-          <button className='border-2 border-primary-500 rounded-xl py-4 px-4 font-black font-junge bg-primary-500 text-white hover:shadow-md hover:-translate-y-1 transition-all'>
-            Book Private Session
-          </button>
-          <div className='lg:w-1/2 w-full h-full flex items-center'>
-            <a href='/contact' className='underline'>
-              Have questions? Reach out now! →
+        <div className='flex w-full flex-col items-center text-[1.1em] font-semibold lg:flex-row lg:gap-6 xl:text-[1.3em]'>
+          <div className=''>
+            <div className='w-[240px] rounded-lg bg-[rgba(255,173,173,1)] p-3 py-4 text-center text-[18px] font-bold text-white transition-all hover:cursor-pointer hover:bg-[rgb(255,193,193)] hover:shadow-lg'>
+              Learn more
+            </div>
+          </div>
+          <div className='flex h-full w-full items-center'>
+            <a
+              href='/contact'
+              className='group flex flex-col transition-all hover:text-[rgba(255,173,173,1)]'
+            >
+              <span className='text-md'>Have questions?</span>
+              <span className='text-sm'>Reach out now! →</span>
             </a>
           </div>
         </div>
       </div>
-      <div className='2xl:w-1/3 md:w-1/2 w-full md:h-full h-[90%] md:mt-0 mt-6 flex justify-center items-center'>
-        <div className='bg-gray-400 sm:w-[80%] w-full sm:h-[70%] h-full max-w-[600px] rounded-[7rem] rounded-tr-none'>
-          <img src="your-image-src.jpg" alt="Your Alt Text" />
-        </div>
+      <div className='aboutImageContent relative z-10 flex h-[500px] w-[500px] flex-col items-start rounded-2xl rounded-bl-[28px] rounded-tl-[74px] bg-[rgb(255,173,173)]/20 p-3'>
+        <img
+          className='rounded-bl-[64px] rounded-br-[12px] rounded-tl-[64px] rounded-tr-[8px]'
+          src='/images/shiren-headshot.jpeg'
+          alt=''
+        />
+        <img className='-mt-12 w-[182px]' src='/images/people.png' alt='' />
       </div>
     </div>
   );
