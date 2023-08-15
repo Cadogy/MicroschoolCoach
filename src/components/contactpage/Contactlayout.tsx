e25e866c8bb338166e2a4678db1d34bb1e51812d
Interborn <dylansafra@icloud.com>
2023-08-15 09:09:04 -0400

    Minor changes

--- src/components/contactpage/Contactlayout.tsx
+++ src/components/contactpage/Contactlayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contactlayout = () => {
   return (
-  <div className="bg-primary-500/30 h-full py-32 flex flex-col items-center my-4 gap-16 text-gray-700">
+  <div className="bg-primary-500/30 h-full py-32 flex flex-col items-center mt-2 gap-16 text-gray-700">
     <div className='w-full flex flex-col justify-center items-center gap-6'>
         <h1 className='text-[3em] font-junge'>Got <span className='text-primary-500'>Questions</span>? I’ve got <span className='text-primary-500'>Answers</span>.</h1>
         <p className='w-2/3 text-[1.5em] leading-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore. </p>
--- src/components/global/Navbar/Navbar.tsx
+++ src/components/global/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className='w-full h-[10vh] flex'>
+    <div className='w-full h-[10vh] flex relative z-[999]'>
       <div className='w-1/6 flex justify-center items-center translate-x-8 translate-y-4'>
         <a href="/home">
           <Image src={logo1} alt="" className='w-full' />
@@ -12,12 +12,12 @@ const Navbar = () => {
       </div>
       <div className='w-full h-full flex items-center font-junge font-medium text-[20px] text-gray-700'>
         <div className='w-full h-full flex justify-center items-center divide-x-2'>
-          <a href="/about" className="px-10 hover:-translate-y-1 hover:text-primary-500 transition-all">About</a>
-          <a href="/blog" className="px-10 hover:-translate-y-1 hover:text-primary-500 transition-all">Blog</a>
-          <a href="/contact" className="px-10 hover:-translate-y-1 hover:text-primary-500 transition-all">Contact</a>
-          <a href="/resources" className="px-10 hover:-translate-y-1 hover:text-primary-500  transition-all">Resources</a>
+          <a href="/about" className="px-10 hover:text-primary-500 transition-all">About</a>
+          <a href="/blog" className="px-10 hover:text-primary-500 transition-all">Blog</a>
+          <a href="/contact" className="px-10 hover:text-primary-500 transition-all">Contact</a>
+          <a href="/resources" className="px-10 hover:text-primary-500  transition-all">Resources</a>
         </div>
-        <button className='w-full bg-primary-500 hover:bg-primary-400 px-8 py-4 mr-10 rounded-xl font-medium text-white font-primary hover:shadow-xl transition-all max-w-[200px]'>
+        <button className='bg-primary-500 hover:bg-primary-400 mr-10 text-white font-primary hover:shadow-md transition-all max-w-[200px] w-[240px] rounded-lg  p-3 py-4 text-center text-[18px] font-bold'>
           Book Now
         </button>
       </div>
